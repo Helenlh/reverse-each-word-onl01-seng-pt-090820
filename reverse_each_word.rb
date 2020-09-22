caf8b115c4eb5c0e87865b6d1e88d8361ffb6348
HELEN HARMAN <hlharman@bellsouth.net>
2020-09-22 00:47:59 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,7 +6,11 @@ def reverse_each_word(sentence1)
    changed.reverse 
 
 reverse_each_word(sentence2)
+ sentence_array2 = sentence2.split( )
+  sentence_array2.collect do |changed2| 
+   changed2.reverse 
 
+ end.join(" ")
  end.join(" ")
 #return new_sentence.join(" ")
 end 
