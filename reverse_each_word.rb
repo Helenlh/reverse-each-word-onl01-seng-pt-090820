2b1ca64cabf7e39928ee3d158a28b06a3cbfb87c
HELEN HARMAN <hlharman@bellsouth.net>
2020-09-22 00:49:00 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -29,6 +29,7 @@ end
 
 
 
+
   # it 'uses collect' do
   #   expect_any_instance_of(Array).to receive(:collect).and_call_original
   #   reverse_each_word("Verifying that collect is being called.")
